3cdd4531e62c014a5cca40cc52211b07e4762a60
DanielAlonsoBastos <32237029+DanielAlonsoBastos@users.noreply.github.com>
2021-06-04 19:37:16 -0500

    change format strings to support pyhton 3.5

--- hello.py
+++ hello.py
@@ -6,4 +6,4 @@ result = add(1, 2)
 # python 3.5 dont accept the f string
 #print(f"This is the sum: 1, 2, {result}")
 
-print(f"This is the sum: 1, 2, {result}")
+print("This is the sum: 1, 2, %s" % result)
